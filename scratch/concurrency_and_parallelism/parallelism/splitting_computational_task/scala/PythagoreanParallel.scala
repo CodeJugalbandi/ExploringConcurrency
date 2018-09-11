ee159c04669dc96cfb8115f4dd6205b28e202e94
Dhaval Dalal <dhaval@agilefaqs.com>
2018-09-11 23:11:27 +0530

    Made Number of chunks same as number of cores

--- scratch/concurrency_and_parallelism/parallelism/splitting_computational_task/scala/PythagoreanParallel.scala
+++ scratch/concurrency_and_parallelism/parallelism/splitting_computational_task/scala/PythagoreanParallel.scala
@@ -15,12 +15,14 @@ def pythagoreans(start:Int, end: Int) =
   } yield (x,y,z)
 
 def pythagoreansParallel(n: Int) = {
-  val maxChunkSize: Int = 500
-  val numberOfChunks: Int = Math.ceil(n.toFloat / maxChunkSize).toInt
-  val evenlyDistributedChunkSize = n / numberOfChunks
-  // println(s"number of chunks = ${numberOfChunks}, evenlyDistributedChunkSize = ${evenlyDistributedChunkSize}")
+  // We need to have at least as many computational parts as the number of cores
+  val numberOfChunks: Int = Runtime.getRuntime().availableProcessors()
+  val evenlyDistributedChunkSize: Int = Math.floor(n.toFloat / numberOfChunks).toInt
+  println(s"number of chunks = ${numberOfChunks}, evenlyDistributedChunkSize = ${evenlyDistributedChunkSize}")
   val leftOvers: Int = n - evenlyDistributedChunkSize * numberOfChunks
-  // println(s"Left overs = ${leftOvers}")
+  println(s"Left overs = ${leftOvers}")
+  // Having more parts is better than having few, because more parts help keep the cores busy
+  // After a certain number of large parts, having more parts has little benefit
   val futures = (2 to numberOfChunks).scanLeft((1, evenlyDistributedChunkSize)) { (acc, elem) =>
     val (start, end) = acc
     val newStart = end + 1
@@ -30,6 +32,8 @@ def pythagoreansParallel(n: Int) = {
     else 
       (newStart, newEnd)
   } map { case (start, end) =>
+	// Having more threads than the number of cores does not help
+	println((start, end))
     Future { pythagoreans(start, end).toList }
   }
   Future.foldLeft(futures) (List[(Int, Int, Int)]()) { case (acc, list) =>
@@ -39,7 +43,7 @@ def pythagoreansParallel(n: Int) = {
 
 def time[T](f: Future[T]) = {
   val startTime = System.currentTimeMillis  
-  val result = Await.result(f, 3 seconds)
+  val result = Await.result(f, 10 seconds)
   val diff = System.currentTimeMillis - startTime
   println(s"Time Taken = $diff(ms).")
   result
@@ -50,7 +54,7 @@ def time[T](f: Future[T]) = {
 println(time(pythagoreansParallel(10)))   // 31.8ms
 // println(time(pythagoreansParallel(100)))  // 108.8ms
 // println(time(pythagoreansParallel(250)))  // 323.2ms
-// println(time(pythagoreansParallel(500)))  // 1345ms
+// println(time(pythagoreansParallel(539)))  // 1345ms
 // println(time(pythagoreansParallel(750)))  // 1095.8ms
 // println(time(pythagoreansParallel(1000))) // 1890.6ms
 // println(time(pythagoreansParallel(1250))) // 1422.5ms
