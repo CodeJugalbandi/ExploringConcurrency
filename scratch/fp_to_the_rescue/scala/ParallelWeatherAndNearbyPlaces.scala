d6fbca9147b47754212992bec0c9879ebf4c01dd
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2018-10-14 23:04:30 +0530

    Added Asynchronous version using Future and Future-Composition

---
+++ scratch/fp_to_the_rescue/scala/ParallelWeatherAndNearbyPlaces.scala
@@ -0,0 +1,44 @@
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Try, Failure, Success}
+import scala.concurrent.{Future, Promise}
+
+def getData(url: String): Future[String] = {
+  val p = Promise[String]()
+  Future {
+    try {
+      p.success(io.Source.fromURL(url).mkString.trim)	 	
+    } catch {
+      case e: Throwable => p.failure(e)
+    }
+  }
+  p.future	
+} 
+
+// val placesNearbyUrl = s"http://localhost:8000/places/nearby?lat=19.01&lon=72.8&radius=25&unit=km"
+val placesNearbyUrl = s"https://geographic-services.herokuapp.com/places/nearby?lat=19.01&lon=72.8&radius=25&unit=km"
+// val weatherUrl = s"http://localhost:8000/weather?lat=19.01&lon=72.8"
+val weatherUrl = s"https://geographic-services.herokuapp.com/weather?lat=19.01&lon=72.8"
+
+val startTime = System.currentTimeMillis()
+val placesNearbyData = getData(placesNearbyUrl)
+val weatherData = getData(weatherUrl)
+val weatherAndPlacesNearby = for {
+  placesNearby <- placesNearbyData
+  weather <- weatherData
+} yield s"""
+          | {
+		  |   "weather"     : $weather,
+		  |   "placesNearby": $placesNearby
+		  | }
+         """.stripMargin
+
+weatherAndPlacesNearby.onComplete {
+  case Success(data) => {
+	val timeTaken = System.currentTimeMillis() - startTime
+	println(s"Time Taken $timeTaken (ms)")							 
+    println(data)
+  }
+  case Failure(ex) => println(ex.getMessage())
+}
+
+Thread.sleep(8000)
--- scratch/fp_to_the_rescue/scala/SequentialWeatherAndNearbyPlaces.scala
+++ scratch/fp_to_the_rescue/scala/SequentialWeatherAndNearbyPlaces.scala
@@ -1,20 +1,19 @@
-import scala.util.parsing.json._
-import scala.util.Try
+def getData(url: String): String = io.Source.fromURL(url).mkString.trim
 
-val startTime = System.currentTimeMillis()
 // val placesNearbyUrl = s"http://localhost:8000/places/nearby?lat=19.01&lon=72.8&radius=25&unit=km"
 val placesNearbyUrl = s"https://geographic-services.herokuapp.com/places/nearby?lat=19.01&lon=72.8&radius=25&unit=km"
-val placesNearby = io.Source.fromURL(placesNearbyUrl).mkString.trim
 // val weatherUrl = s"http://localhost:8000/weather?lat=19.01&lon=72.8"
 val weatherUrl = s"https://geographic-services.herokuapp.com/weather?lat=19.01&lon=72.8"
-val weather = io.Source.fromURL(weatherUrl).mkString.trim
+
+val startTime = System.currentTimeMillis()
+val placesNearbyData = getData(placesNearbyUrl)
+val weatherData = getData(weatherUrl)
 val timeTaken = System.currentTimeMillis() - startTime
 val weatherAndPlacesNearby = s"""
                                | {
-							   |   "weather"     : $weather,
-							   |   "placesNearby": $placesNearby
+							   |   "weather"     : $weatherData,
+							   |   "placesNearby": $placesNearbyData
 							   | }
                               """.stripMargin
 println(s"Time Taken $timeTaken (ms)")							 
 println(weatherAndPlacesNearby)
-// println(JSON.parseFull(weatherAndPlacesNearby))
