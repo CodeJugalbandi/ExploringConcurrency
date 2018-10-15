2456dd63e8577edf56f5ac9d029e3f052c8db97b
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2018-10-15 18:19:47 +0530

    made sure error works as well

--- scratch/fp_to_the_rescue/javascript/asynchronous_programming.js
+++ scratch/fp_to_the_rescue/javascript/asynchronous_programming.js
@@ -17,12 +17,12 @@ const weatherUrl = "https://geographic-services.herokuapp.com/weather?lat=19.01&
 // const placesNearbyUrl = "http://localhost:8000/places/nearby?lat=19.01&lon=72.8&radius=25&unit=km"
 const placesNearbyUrl = "https://geographic-services.herokuapp.com/places/nearby?lat=19.01&lon=72.8&radius=25&unit=km"
 
-console.time('Time Taken'); 
+console.time('Time Taken');
 Promise.all([getRequestData(weatherUrl), getRequestData(placesNearbyUrl)])
   .then(([weather, placesNearby]) => JSON.parse(`{ "weather": ${weather}, "placesNearby": ${placesNearby} }`))
+  .catch(error => `Could not get data => ${error}`)
   .then(result => {
      console.timeEnd('Time Taken');
 	 console.info(result);
-  })
-  .catch(error => `Could not get data ${error.message}`);
+  });
   
