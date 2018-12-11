1bc71d18020d762394699820d51d76703ec88b7a
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2018-12-11 22:31:30 +0530

    added jar and run.sh

---
+++ session/02_fp_to_the_rescue/scala/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ -f "$1" ]; then
+	/usr/local/bin/scala -classpath "./lib/scala-async-0.97.jar" -feature -J-Xmx4g "${1}"
+	rm *.class
+else
+	echo "usage: ./run.sh <file.scala>"
+	exit 0
+fi
+
