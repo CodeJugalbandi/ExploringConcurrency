7ec20952e3e6e5bcc87dba06307518fe3addce67
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2018-12-14 09:34:44 +0530

    made runnable

--- session/02_fp_to_the_rescue/scala/run.sh
+++ session/02_fp_to_the_rescue/scala/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -f "$1" ]; then
-	/usr/local/bin/scala -classpath "./lib/scala-async-0.97.jar" -feature -J-Xmx4g "${1}"
-	rm *.class
+	/usr/local/bin/scala -classpath "./scala/lib/scala-async-0.97.jar" -feature -J-Xmx4g "${1}"
+	rm *.class&>/dev/null
 else
 	echo "usage: ./run.sh <file.scala>"
 	exit 0
