cd9be5909917906d666266d26bf6bf133ed3c1b5
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2018-12-14 10:18:31 +0530

    added delay

--- session/02_fp_to_the_rescue/scala/_06_ParallelAsynchronousUsingAsyncAwait.scala
+++ session/02_fp_to_the_rescue/scala/_06_ParallelAsynchronousUsingAsyncAwait.scala
@@ -4,6 +4,11 @@ import scala.concurrent.duration.{Duration}
 import scala.util.{Try, Success, Failure}
 import scala.async.Async.{async, await}
 
+def doSomeOtherImportantStuff = {
+  println("doing important stuff...")
+  Thread.sleep(5000)
+}
+
 def getRequestData(url: String): Future[String] = async {
   io.Source.fromURL(url).mkString.trim
 } 
@@ -15,7 +20,6 @@ val lat = "lat=19.01"; val lon = "lon=72.8"; val radius = "radius=25"; val units
 val placesNearbyUrl = s"$host$nearbyPath?$lat&$lon&$radius&$units";
 val weatherUrl = s"$host$weatherPath?$lat&$lon";
 
-val startTime = System.currentTimeMillis
 val weatherAndPlacesNearby = async {
   val weatherFuture = getRequestData(weatherUrl)
   val placesNearbyFuture = getRequestData(placesNearbyUrl)
@@ -23,12 +27,10 @@ val weatherAndPlacesNearby = async {
 }
 
 weatherAndPlacesNearby.onComplete {
-  case Success(data) => {
-    val timeTaken = System.currentTimeMillis - startTime
-    println(s"Time Taken $timeTaken (ms)")
-    println(data)
-  }
+  case Success(data) => println(data)
   case Failure(e) => println(e.getMessage)
 }
+
+doSomeOtherImportantStuff
 
 Await.result(weatherAndPlacesNearby, Duration.Inf) // Wait for results, only for this example for onComplete to get called, not in practice
